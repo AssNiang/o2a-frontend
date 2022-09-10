6012b3386b97803750e628c1335e5f87ce0d5a2b
Ass NIANG <nianga@ept.sn>
2022-09-10 22:50:10 +0000

    deploying to azure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
@@ -15,7 +15,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { RightSideBarComponent } from './components/right-side-bar/right-side-bar.component';
 import { LeftSideBarComponent } from './components/left-side-bar/left-side-bar.component';
 import { ContentComponent } from './components/content/content.component';
-import { routingComponents } from "./app-routing.module";
 import { HomeAppliComponent } from './components/home-appli/home-appli.component';
 import { PostItemComponent } from './components/post-item/post-item.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
