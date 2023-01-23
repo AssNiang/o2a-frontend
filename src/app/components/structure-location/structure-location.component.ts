2a149f07089c4929f2ec3265835cebcc7e917fbf
Ass NIANG <nianga@ept.sn>
2023-01-23 02:16:59 +0000

    add and get health structures

--- src/app/components/structure-location/structure-location.component.ts
+++ src/app/components/structure-location/structure-location.component.ts
@@ -1,6 +1,11 @@
+import { NgForOf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AppComponent } from 'src/app/app.component';
+import { Health_struct } from 'src/app/models/health_struct';
+import { User } from 'src/app/models/user';
+import { HealthStructService } from 'src/app/shared/services/health-struct.service';
 import { UserService } from 'src/app/shared/services/user.service';
 import { LeftSideBarComponent } from '../left-side-bar/left-side-bar.component';
 
@@ -13,8 +18,13 @@ declare const L: any;
 })
 export class StructureLocationComponent implements OnInit {
   user_id: string = '';
+  is_admin: boolean = false;
 
-  constructor(private router: Router, private _userService: UserService) {}
+  constructor(
+    private router: Router,
+    private _userService: UserService,
+    private _healthStructService: HealthStructService
+  ) {}
 
   ngOnInit(): void {
     this.user_id = this.router.url.split('/')[2];
@@ -25,6 +35,7 @@ export class StructureLocationComponent implements OnInit {
         AppComponent.typeUser = LeftSideBarComponent.typeUser = 'specialist';
       } else if (user.is_admin) {
         AppComponent.typeUser = LeftSideBarComponent.typeUser = 'admin';
+        this.is_admin = true;
       } else {
         AppComponent.typeUser = LeftSideBarComponent.typeUser = 'connected';
       }
@@ -37,60 +48,86 @@ export class StructureLocationComponent implements OnInit {
 
     navigator.geolocation.getCurrentPosition(
       (position: GeolocationPosition) => {
-        console.log(
-          `lat: ${position.coords.latitude}, long: ${position.coords.longitude}`
-        );
+        // console.log(
+        //   `lat: ${position.coords.latitude}, long: ${position.coords.longitude}`
+        // );
 
         let latLong = [position.coords.latitude, position.coords.longitude];
-        let structuresDeSante: { name: string; coords: number[] }[] = [
-          {
-            name: 'service Medical EPT',
-            coords: [14.79759710124241, -16.967394452231684],
-          },
-          {
-            name: 'Laboratoire National De Santé Publique Thies',
-            coords: [14.793898530303617, -16.934326696057074],
-          },
-          {
-            name: 'Institut Santé Multi Services',
-            coords: [14.791687882207437, -16.93231344256073],
-          },
-          {
-            name: 'Poste de Santé Sainte Anne',
-            coords: [14.80054264648186, -16.929123533899006],
-          },
-          {
-            name: 'Centre De Santé 10ème De Thiès',
-            coords: [14.794363679206366, -16.925522327098545],
-          },
-          {
-            name: 'District sanitaire de thies Poste de sante de mbour1',
-            coords: [14.776985246064234, -16.943613431493993],
-          },
-        ];
+        this._healthStructService
+          .getAllHealth_struct()
+          .subscribe((health_struct) => {
+            let map = L.map('map').setView(latLong, 13);
 
-        let map = L.map('map').setView(latLong, 13);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+              maxZoom: 19,
+              attribution:
+                '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+            }).addTo(map);
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          maxZoom: 19,
-          attribution:
-            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        }).addTo(map);
+            let marker = L.marker(latLong).addTo(map);
+            marker.bindPopup('Votre position').openPopup();
 
-        let marker = L.marker(latLong).addTo(map);
-        marker.bindPopup('Votre position').openPopup();
+            for (let structure of health_struct) {
+              let circle = L.circle([structure.latitude, structure.longitude], {
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 0.5,
+                radius: 100,
+              }).addTo(map);
+              circle.bindPopup(
+                `${structure.name} <br> Latitude: ${structure.latitude} <br> Longitude: ${structure.longitude}`
+              );
+            }
+          });
+        // let structuresDeSante: { name: string; coords: number[] }[] = [
+        //   {
+        //     name: 'service Medical EPT',
+        //     coords: [14.79759710124241, -16.967394452231684],
+        //   },
+        //   {
+        //     name: 'Laboratoire National De Santé Publique Thies',
+        //     coords: [14.793898530303617, -16.934326696057074],
+        //   },
+        //   {
+        //     name: 'Institut Santé Multi Services',
+        //     coords: [14.791687882207437, -16.93231344256073],
+        //   },
+        //   {
+        //     name: 'Poste de Santé Sainte Anne',
+        //     coords: [14.80054264648186, -16.929123533899006],
+        //   },
+        //   {
+        //     name: 'Centre De Santé 10ème De Thiès',
+        //     coords: [14.794363679206366, -16.925522327098545],
+        //   },
+        //   {
+        //     name: 'District sanitaire de thies Poste de sante de mbour1',
+        //     coords: [14.776985246064234, -16.943613431493993],
+        //   },
+        // ];
 
-        for (let structure of structuresDeSante) {
-          let circle = L.circle(structure.coords, {
-            color: 'red',
-            fillColor: '#f03',
-            fillOpacity: 0.5,
-            radius: 100,
-          }).addTo(map);
-          circle.bindPopup(
-            `${structure.name} <br> Latitude: ${structure.coords[0]} <br> Latitude: ${structure.coords[1]}`
-          );
-        }
+        // let map = L.map('map').setView(latLong, 13);
+
+        // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        //   maxZoom: 19,
+        //   attribution:
+        //     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        // }).addTo(map);
+
+        // let marker = L.marker(latLong).addTo(map);
+        // marker.bindPopup('Votre position').openPopup();
+
+        // for (let structure of structuresDeSante) {
+        //   let circle = L.circle(structure.coords, {
+        //     color: 'red',
+        //     fillColor: '#f03',
+        //     fillOpacity: 0.5,
+        //     radius: 100,
+        //   }).addTo(map);
+        //   circle.bindPopup(
+        //     `${structure.name} <br> Latitude: ${structure.coords[0]} <br> Latitude: ${structure.coords[1]}`
+        //   );
+        // }
 
         //this.watchPostion(14.7942, -16.9649, .0001);
       }
@@ -117,5 +154,24 @@ export class StructureLocationComponent implements OnInit {
         maximumAge: 0,
       }
     );
+  }
+
+  submit(health_struct: NgForm) {
+    try {
+      this._healthStructService
+        .addHealth_struct(health_struct.value)
+        .subscribe((h_struct) => {
+          this.reloadComponent();
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  reloadComponent() {
+    let currentUrl = this.router.url;
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.router.navigate([currentUrl]);
+    });
   }
 }
